6f7617aef99524be9dfae9e000ab4fb519b34fbd
nixberg <k.nixberg@gmail.com>
2020-09-15 01:49:27 +0200

    Update genpass to 0.4.0

--- Formula/genpass.rb
+++ Formula/genpass.rb
@@ -1,8 +1,8 @@
 class Genpass < Formula
   desc "Simple, opinionated password generator"
   homepage "https://github.com/nixberg/genpass-swift"
-  url "https://github.com/nixberg/genpass-swift/archive/0.3.0.tar.gz"
-  sha256 "368c34d7c364eb3a90fd18f44c30cc96ffe61f0c06e47d2e06c60f5fa0b47cd7"
+  url "https://github.com/nixberg/genpass-swift/archive/0.4.0.tar.gz"
+  sha256 "42aaaf3ef0dbb0c0debf72656ad1fe9592ca40ba5100b493590ecb9ea516af29"
   
   def install
     system "swift", "build",
