81fd7e4f355c1e38dc7d851414529aca975b2d7b
nixberg <k.nixberg@gmail.com>
2021-09-04 11:41:35 +0200

    Update checkpass.rb

--- Formula/checkpass.rb
+++ Formula/checkpass.rb
@@ -1,8 +1,8 @@
 class Checkpass < Formula
   desc "Check passwords against the Pwned Passwords API"
   homepage "https://github.com/nixberg/checkpass-swift"
-  url "https://github.com/nixberg/checkpass-swift/archive/0.2.0.tar.gz"
-  sha256 "b42b12fa4c38926a33700e6eb235c9ec5da01e9999a4bb387342903d9df30050"
+  url "https://github.com/nixberg/checkpass-swift/archive/0.3.0.tar.gz"
+  sha256 "9270f4e6f9026cb2918634a40f2834b24e924299c5624e45e0f7fd35cdce7c24"
   
   def install
     system "swift", "build",
