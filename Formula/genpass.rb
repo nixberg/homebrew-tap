70f0003b20a13aab600dd383d822246be248b6a7
nixberg <nixberg@users.noreply.github.com>
2022-03-18 14:23:59 +0100

    Update genpass.rb

--- Formula/genpass.rb
+++ Formula/genpass.rb
@@ -1,8 +1,8 @@
 class Genpass < Formula
   desc "Simple password generator"
   homepage "https://github.com/nixberg/genpass-swift"
-  url "https://github.com/nixberg/genpass-swift/archive/0.6.1.tar.gz"
-  sha256 "c203475cdf336e6589f441a711bc5e6464600a588ca8ffaa3a61eb5eba674b5c"
+  url "https://github.com/nixberg/genpass-swift/archive/0.7.0.tar.gz"
+  sha256 "93d2c3ea35690c1ce54bf3de44e4b3109974704388773eea20cb5cbb3d548ba1"
   
   def install
     system "swift", "build",
