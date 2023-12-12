652b3367db44f44d66f88780adf6e759ef1356ef
nixberg <310825+nixberg@users.noreply.github.com>
2023-12-12 17:43:29 +0100

    Update genpass.rb

--- Formula/genpass.rb
+++ Formula/genpass.rb
@@ -1,28 +1,29 @@
 class Genpass < Formula
   desc "Simple password generator"
   homepage "https://github.com/nixberg/genpass-swift"
-  url "https://github.com/nixberg/genpass-swift/archive/0.8.0.tar.gz"
-  sha256 "921ff6aed274f658f9fae55fdbc94a24bbeba88447ac9fc1df6b402642738dae"
+  url "https://github.com/nixberg/genpass-swift.git".
+      tag:      "0.8.0",
+      revision: "33c510166279e223a5396430a761067e0f1353a2"
+  license "MIT"
   head "https://github.com/nixberg/genpass-swift.git", branch: "main"
+  
+  depends_on xcode: ["15.0", :build]
+  uses_from_macos "swift"
 
   def install
     system "swift", "build",
-        "--product", "genpass",
         "--configuration", "release",
         "--disable-sandbox"
+        "--product", "genpass",
     bin.install ".build/release/genpass"
     
-    generate_completion_script "bash", "genpass.bash"
+    system "#{bin/"genpass"} --generate-completion-script bash > genpass.bash"
     bash_completion.install "genpass.bash"
     
-    generate_completion_script "fish", "genpass.fish"
+    system "#{bin/"genpass"} --generate-completion-script fish > genpass.fish"
     fish_completion.install "genpass.fish"
     
-    generate_completion_script "zsh", "_genpass"
+    system "#{bin/"genpass"} --generate-completion-script zsh > _genpass"
     zsh_completion.install "_genpass"
-  end
-  
-  def generate_completion_script (shell, file)
-    system "#{bin/"genpass"} --generate-completion-script #{shell} > #{file}"
   end
 end
