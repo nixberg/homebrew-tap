8a3b37dd3079be2b1ed2a9739991a65ac492b6b8
nixberg <310825+nixberg@users.noreply.github.com>
2024-01-05 15:27:45 +0100

    Update genpass.rb

--- Formula/genpass.rb
+++ Formula/genpass.rb
@@ -6,24 +6,18 @@ class Genpass < Formula
       revision: "33c510166279e223a5396430a761067e0f1353a2"
   license "MIT"
   head "https://github.com/nixberg/genpass-swift.git", branch: "main"
-  
+
   depends_on xcode: ["15.0", :build]
   uses_from_macos "swift"
 
   def install
     system "swift", "build",
-        "--configuration", "release",
-        "--disable-sandbox",
-        "--product", "genpass"
+           "--configuration", "release",
+           "--disable-sandbox",
+           "--product", "genpass"
+
     bin.install ".build/release/genpass"
-    
-    system "#{bin/"genpass"} --generate-completion-script bash > genpass.bash"
-    bash_completion.install "genpass.bash"
-    
-    system "#{bin/"genpass"} --generate-completion-script fish > genpass.fish"
-    fish_completion.install "genpass.fish"
-    
-    system "#{bin/"genpass"} --generate-completion-script zsh > _genpass"
-    zsh_completion.install "_genpass"
+
+    generate_completions_from_executable bin/"genpass", "--generate-completion-script"
   end
 end
