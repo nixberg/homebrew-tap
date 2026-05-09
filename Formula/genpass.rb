45542ed9e8e54ff8c8bd147d910a10995fb09eb3
nixberg <310825+nixberg@users.noreply.github.com>
2026-05-09 16:37:31 +0200

    Update genpass.rb

--- Formula/genpass.rb
+++ Formula/genpass.rb
@@ -2,22 +2,14 @@ class Genpass < Formula
   desc "Simple password generator"
   homepage "https://github.com/nixberg/genpass-swift"
   url "https://github.com/nixberg/genpass-swift.git",
-      tag:      "0.9.0",
-      revision: "4bcb7efe368f8ec7c5dce7663ce09fe4463c7634"
+      tag:      "0.10.0",
+      revision: "06b0f537ac94a832ebd23f260b2e4bf201935c0d"
   license "MIT"
   head "https://github.com/nixberg/genpass-swift.git", branch: "main"
 
-  depends_on xcode: ["15.0", :build]
-  uses_from_macos "swift"
-
   def install
-    system "swift", "build",
-           "--configuration", "release",
-           "--disable-sandbox",
-           "--product", "genpass"
-
-    bin.install ".build/release/genpass"
-
-    generate_completions_from_executable bin/"genpass", "--generate-completion-script"
+    system "swift", "build", "--configuration=release", "--disable-sandbox", "--product=#{name}"
+    bin.install ".build/release/#{name}"
+    generate_completions_from_executable bin/name, "--generate-completion-script"
   end
 end
